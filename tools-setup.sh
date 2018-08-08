f071fd448df97a1ca178cfde1d3fa0c786b6cb86
Clifford Carnmo <clifford@carnmo.com>
2018-08-08 11:16:04 +0200

    Cleanup

--- tools-setup.sh
+++ tools-setup.sh
@@ -3,11 +3,10 @@ cd src/pucrunch && make && cd ../../
 cd src/genosine && make && cd ../../
 
 mkdir bin
-cp src/acme/src/acme bin/
-cp src/pucrunch/pucrunch bin/
-cp src/genosine/genosine bin/
+mv src/acme/src/acme bin/
+mv src/pucrunch/pucrunch bin/
+mv src/genosine/genosine bin/
 
 sudo cp -rv c64/* /usr/lib/vice/
 ln -s /usr/bin/x64 bin/x64
-
 
