14bd99332f28b496bc65085909ace610e474a862
Clifford Carnmo <clifford@carnmo.com>
2018-08-08 11:07:31 +0200

    Update

---
+++ tools-setup.sh
@@ -0,0 +1,15 @@
+cd src/acme/src/ && make && cd ../../../
+cd src/pucrunch && make && cd ../../
+cd src/genosine && make && cd ../../
+
+mkdir bin
+cp src/acme/src/acme bin/
+cp src/pucrunch/pucrunch bin/
+cp src/genosine/genosine bin/
+
+sudo cp -rv c64/* /usr/lib/vice/
+ln -s /usr/bin/x64 bin/x64
+
+rm -rf src
+rm -rf c64
+
